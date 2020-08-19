9cac53355de68fe27a71343c113f64b68951357b
ktc312 <ktc312@nyu.edu>
2020-08-19 17:23:23 -0400

    wip basic turn components

--- mahjong/kyoku.py
+++ mahjong/kyoku.py
@@ -14,6 +14,31 @@ class Turn:
     the wall and makes a discard, unless the hand is a winning hand with
     the declaration of tsumo. see flowchart in README
     """
+    def __init__(self):
+        # Honcha 「本家」, player who discarded tile (好像沒有這個詞，我發明的)
+        # Shimocha 「下家」, or player to the right of discard tile player
+        # Toimen 「対面」, or player across discard tile player
+        # Kamicha 「上家」, or player to the left of discard tile player
+        # discard tile
+        pass
+
+    def discard_tile_handle(self):
+        # any player can Ron? if yes, end of Kyoku
+        # any palyer can Kan? if yes, draw a tile from rinshanpai
+        # any palyer can Pon? if yes, choose_discard_tile()
+        # can Shimocha Chii? if yes, choose_discard_tile()
+        # Otherwise, Shimocha draw a tile from playling_wall
+        pass
+
+    def tile_in_handle(self):
+        # can player tsumo? if yes, end of Kyoku
+        # can player Kan? if yes, draw a tile from rinshanpai
+        # Otherwise, choose_discard_tile()
+        pass
+
+    def choose_discard_tile(self):
+        # can riichi?
+        pass
 
 
 class Kyoku:
